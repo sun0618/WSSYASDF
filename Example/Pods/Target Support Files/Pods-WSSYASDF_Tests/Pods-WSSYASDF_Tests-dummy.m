3f81d6cf3bbda0425d2c6b8000ec8f7f52837275
sun0618 <1679290828@qq.com>
2023-06-02 17:14:58 +0800

    as

---
+++ Example/Pods/Target Support Files/Pods-WSSYASDF_Tests/Pods-WSSYASDF_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WSSYASDF_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WSSYASDF_Tests
+@end
---
+++ Example/Pods/Target Support Files/WSSYASDF/WSSYASDF-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WSSYASDF : NSObject
+@end
+@implementation PodsDummy_WSSYASDF
+@end
